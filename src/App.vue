2e0a2644ad7ad34321cfc3a05ef5634dd775f7bc
Anyway521 <2411535738@qq.com>
2022-05-23 15:48:13 +0800

    [update]更新demo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// import SimpleTable from "./components/table/index";
-// import Base from '@/demo/base.vue';
-// import NoPaging from '@/demo/noPaging.vue'
-// import SetHeight from "@/demo/setHeight.vue";
-import Sort from "@/demo/sort.vue";
+import Demo from "@/demo/index.vue";
 
 </script>
 
 <template>
-  <Sort />
+  <Demo />
 </template>
 
 <style>
