66660643fae010fe6c00f7e32f2f56e0080271c0
Chris <43142096+Anyway521@users.noreply.github.com>
2022-05-28 14:14:36 +0800

    [updatetest]

---
+++ test/pagination.test.ts
@@ -0,0 +1,33 @@
+import { shallowMount } from "@vue/test-utils";
+import { describe, expect, test } from "vitest";
+import Pagination from "../src/components/pagination/index.vue";
+import { usePagination } from '../src/components/pagination/usePagination';
+
+describe("mount table", () => {
+    const wrapper = shallowMount(Pagination, {
+        props: {
+            total: 5,
+            pageSize: 10,
+        },
+    });
+    // 不配置pageAble默认为true
+    test('pagable', () => {
+        expect(wrapper.props().enable).toBe(true)
+    });
+
+    test('total', () => {
+        expect(wrapper.props().total).toBe(5)
+    })
+});
+
+describe("test hooks", () => {
+    const context = usePagination({
+        total: 5,
+        pageSize: 10,
+        enable: false
+    })
+
+    test('test', () => {
+        expect(context.enable.value).toBe(false)
+    })
+})
